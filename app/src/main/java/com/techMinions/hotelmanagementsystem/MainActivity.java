91d6470345032076ad9aa8d36aca3e01f9a92727
thushaltk <thushal1999@gmail.com>
2020-08-13 19:16:44 +0530

    Thushal Commit 1

--- app/src/main/java/com/techMinions/hotelmanagementsystem/MainActivity.java
+++ app/src/main/java/com/techMinions/hotelmanagementsystem/MainActivity.java
@@ -6,24 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnmain;
+    ImageButton roomBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnmain = findViewById(R.id.btnmain);
+        roomBtn = findViewById(R.id.roomBtn);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        btnmain.setOnClickListener(new View.OnClickListener() {
+        roomBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent myI = new Intent(MainActivity.this, rooms.class);
