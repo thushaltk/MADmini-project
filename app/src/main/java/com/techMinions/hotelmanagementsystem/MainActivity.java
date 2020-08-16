0a50d0aa6b2ce97678adc01f4f1d262727f4e0a0
sujanichathurika <chathusujani57@gmail.com>
2020-08-16 10:43:10 +0530

    Sujani Commit 1

--- app/src/main/java/com/techMinions/hotelmanagementsystem/MainActivity.java
+++ app/src/main/java/com/techMinions/hotelmanagementsystem/MainActivity.java
@@ -6,30 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton roomBtn;
+    Button btnmain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        roomBtn = findViewById(R.id.roomBtn);
+        btnmain = findViewById(R.id.btnmain);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        roomBtn.setOnClickListener(new View.OnClickListener() {
+        btnmain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent myI = new Intent(MainActivity.this, rooms.class);
                 startActivity(myI);
             }
         });
+
+
     }
 }
---
+++ app/src/main/java/com/techMinions/hotelmanagementsystem/bookedhallsdply.java
@@ -0,0 +1,14 @@
+package com.techMinions.hotelmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bookedhallsdply extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookedhallsdply);
+    }
+}
